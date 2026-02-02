5c58a9625567943b86657d4aac56d6fcf86c85b5
eokaka2007 <guimaraesm.kaua@gmail.com>
2026-02-02 11:06:07 -0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './index.css'
-import App from './App.tsx'
+import Login from './pages/Login.tsx'
 import Dashboard from './pages/Dashboard.tsx'
 import PrivateRoute from './routes/PrivateRoute.tsx'
 
@@ -10,7 +10,10 @@ createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<App />} />
+        {/* Rota principal agora usa o componente Login com fundo 3D */}
+        <Route path="/" element={<Login />} />
+        
+        {/* Rota protegida */}
         <Route 
           path="/dashboard" 
           element={
