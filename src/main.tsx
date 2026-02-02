d853bc7ead413087729b7775389ec403d5cd2fa4
eokaka2007 <guimaraesm.kaua@gmail.com>
2026-02-02 10:59:54 -0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from 'react-router-dom' // Adicionado
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './index.css'
-import App from './App.js'
-import Dashboard from './pages/Dashboard.js'
-import PrivateRoute from './components/PrivateRoute.js'
+import App from './App.tsx'
+import Dashboard from './pages/Dashboard.tsx'
+import PrivateRoute from './routes/PrivateRoute.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
-        {/* Rota da tela de Login */}
         <Route path="/" element={<App />} />
-        
-        {/* Rota Protegida do Dashboard */}
         <Route 
           path="/dashboard" 
           element={
