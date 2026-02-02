07caf64c7a08ac6ca1b3933d5b31775792cf3671
eokaka2007 <guimaraesm.kaua@gmail.com>
2026-02-02 10:56:22 -0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom' // Adicionado
 import './index.css'
-import App from './App.tsx'
-import Dashboard from './pages/Dashboard.tsx'
-import PrivateRoute from './components/PrivateRoute'
+import App from './App.js'
+import Dashboard from './pages/Dashboard.js'
+import PrivateRoute from './components/PrivateRoute.js'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
